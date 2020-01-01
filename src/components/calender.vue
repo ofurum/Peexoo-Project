5ded07757e3fcba55fa9fda4f9b42ec3298a0821
Ofurum chiagoziem <chiagoziemofurum20@gmail.com>
2020-01-01 13:18:40 +0100

    git push -m "commit project"

--- src/components/calender.vue
+++ src/components/calender.vue
@@ -1,14 +1,8 @@
 <template>
     <div>
          <div class="calender">
-             <div class="calender-header">
-                <i class = "fa fa-fw fa-chevron-left" @click="subtractMonth"></i>
-                <h4> {{month + '-'+ year }}</h4>
-                <i class="fa fa-fw fa-cheveron-right" @click="addMonth">
-             </div>
-             <ul class="weekdays">
-                 <li v-for = "day in days" :key= day></li>
-             </ul>
+           
+
          </div>
     </div>
 </template>
@@ -18,67 +12,6 @@
 
 
 <script>
-import moment from 'moment.js'
-
-export default {
-    data(){
-        return{
-            today: moment(),
-            dateContext: moment(),
-            days: ['S', 'M','T', 'W', 'T', 'F', 'S']
-        }
-    },
-    computed: {
-        year: function() {
-            var t= this;
-            return t.dateContext.format('Y');
 
 
-        },
-        month: function () {
-            var t = this;
-            return t.dateContext.format('MMMM');
-
-
-        },
-        daysInMonth: function () {
-              var t = this;
-              return t.dateContext.daysInMonth();
-        },
-        currentDate: function () {
-              var t = this;
-              return t.dateContext.get('date')
-        },
-        firstDayOfTheMonth: function () {
-              var t = this;
-              var firstDay = moment(t.dateContext).subtract((t.currentDate-1), 'days');
-              return firstDay.weekday();
-        },
-        initialDate: function () {
-            var b = this;
-            return b.today.get('date');
-        },
-        initialMonth: function () {
-            var b = this;
-            return t.today.format('MMM');
-
-        },
-        initialYear: function () {
-            var b = this;
-            return b.today.format('Y');
-        }
-    },
-
-    methods: {
-        addMonth: function () {
-            var c = this;
-            c.dateContext = moment(c.dateContext).add(1, 'month');
-        },
-        subtractMonth: function () {
-            var c = this;
-            c.dateContext = moment(c.dateContext).subtract(1, 'month')
-        }
-    }
-    
-}
 </script>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon }  from '@fortawesome/vue-fontawesome'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
+// import FunctionalCalendar from 'vue-functional-calendar';
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
@@ -14,6 +15,10 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 // Vue.use(Bootstrap)
 library.add(faUserSecret)
 Vue.use(BootstrapVue)
+Vue.use(FunctionalCalendar, {
+  dayNames: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
+});
+
 
  
 Vue.component('font-awesome-icon', FontAwesomeIcon)
@@ -24,6 +29,6 @@ new Vue({
   store,
   // FontAwesomeIcon ,
   // faUserSecret,
-
+  FunctionalCalendar,
   render: h => h(App)
 }).$mount('#app')
